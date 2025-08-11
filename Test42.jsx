d25c73ec0fa25c483a2ce79c761ddef4fd373bbc
RoundLab <gsg4821@gmail.com>
2025-08-11 11:30:50 +0900

    추가: Test42.jsx 파일 생성 및 테스트

--- Test42.jsx
+++ Test42.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Test42 = () => {
-    return <div></div>;
+    return (
+        <div>
+            <h2>안녕</h2>
+        </div>
+    );
 };
 
 export default Test42;
