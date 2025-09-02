27f8d000a1c3e6d5fbef0bb85771163001ea6515
mirella2-3 <zmeo89@gmail.com>
2025-09-02 11:14:40 +0900

    kakao

--- src/store/modules/authSlice.js
+++ src/store/modules/authSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+/* import { createSlice } from '@reduxjs/toolkit';
 
 const memberData = [
     {
@@ -60,5 +60,62 @@ export const authSlice = createSlice({
     },
 });
 
+export const authActions = authSlice.actions;
+export default authSlice.reducer; */
+
+import { createSlice } from '@reduxjs/toolkit';
+const memberData = [
+    {
+        id: 1,
+        name: 'af',
+        tel: '010-1111-2222',
+        password: 'a1234',
+        loginId: 'af',
+    },
+];
+const initialState = {
+    members: localStorage.getItem('members')
+        ? JSON.parse(localStorage.getItem('members'))
+        : memberData,
+    authed: localStorage.getItem('authed') ? JSON.parse(localStorage.getItem('authed')) : false,
+    user: localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null,
+};
+let no = initialState.members.length + 1;
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.authed = true;
+            state.user = {
+                name: action.payload.name,
+                loginId: action.payload.loginId,
+            };
+        },
+        loginSuccess: (state, action) => {
+            // 카카오 로그인용
+            const user = action.payload;
+            state.authed = true;
+            state.user = user;
+            localStorage.setItem('authed', JSON.stringify(true));
+            localStorage.setItem('user', JSON.stringify(user));
+        },
+        logout: (state) => {
+            state.authed = false;
+            state.user = null;
+            localStorage.setItem('authed', JSON.stringify(false));
+            localStorage.removeItem('user');
+        },
+        signup: (state, action) => {
+            action.payload.id = no++;
+            state.members.push({
+                ...action.payload,
+                loginId: action.payload.loginId,
+                password: action.payload.password,
+            });
+            localStorage.setItem('members', JSON.stringify(state.members));
+        },
+    },
+});
 export const authActions = authSlice.actions;
 export default authSlice.reducer;
